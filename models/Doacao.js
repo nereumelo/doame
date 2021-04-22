ed5f469eecc5ec01614723d7d29d1707c8bfca23
marianaRodLima <marianarlima93@gmail.com>
2021-04-22 14:11:07 -0300

    Adicionando rota, model e controller de doação

---
+++ models/Doacao.js
@@ -0,0 +1,22 @@
+
+ module.exports = (sequelize, DataTypes) => {
+
+    const Doacao = sequelize.define(
+      'Doacao', {
+        parceiros_id: DataTypes.INTEGER,
+        doadores_id: DataTypes.INTEGER,
+        valor: DataTypes.DOUBLE,
+        forma_pagamento: DataTypes.STRING(45),
+        status: {
+            type: DataTypes.ENUM,
+            values: ['Em andamento', 'Conluído','Não realizado']
+        },
+        created_at: DataTypes.DATE,
+      }, {
+        tableName: "doacoes",
+        timestamps: false
+      }
+    );
+  
+    return Doacao;
+  }
