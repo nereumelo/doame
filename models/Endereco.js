6aecd34ce1548bc629508848bf69411ec183bea5
nvmelo <nereuvictor1@gmail.com>
2021-04-22 22:15:32 -0300

    Implementando as associations de models

--- models/Endereco.js
+++ models/Endereco.js
@@ -2,27 +2,24 @@ module.exports = (sequelize, DataTypes) => {
 
     const Endereco = sequelize.define(
         "Endereco", {
-            parceiros_id: DataTypes.INTEGER,
-            pais: DataTypes.STRING(100),
-            estado: DataTypes.STRING(100),
-            cidade: DataTypes.STRING(100),
-            bairro: DataTypes.STRING(100),
-            logradouro: DataTypes.STRING(200),
-            cep: DataTypes.STRING(20),
-            numero: DataTypes.STRING(10),
-            complemento: DataTypes.STRING(150)
-        }, {
-            tableName: "enderecos",
-            timestamps: false
-        }
+        parceiros_id: DataTypes.INTEGER,
+        pais: DataTypes.STRING(100),
+        estado: DataTypes.STRING(100),
+        cidade: DataTypes.STRING(100),
+        bairro: DataTypes.STRING(100),
+        logradouro: DataTypes.STRING(200),
+        cep: DataTypes.STRING(20),
+        numero: DataTypes.STRING(10),
+        complemento: DataTypes.STRING(150)
+    }, {
+        tableName: "enderecos",
+        timestamps: false
+    }
     );
 
-    //está comentado porque, falta fazer na tabela Parceiros a associação.
-
-   /* Endereco.associate = (models) => {
-        //relação 1:1 (endereço tem um pareceiro)
-        Endereco.belongsTo(models.Parceiro, {as: "parceiros", foreignKey: "enderecos_id"});
-    }*/
+    Endereco.associate = (models) => {
+        Endereco.belongsTo(models.Parceiro, { as: "parceiro", foreignKey: "parceiros_id" });
+    };
 
     return Endereco;
 }
--- models/Parceiro.js
+++ models/Parceiro.js
@@ -1,18 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-    
+
     const Parceiro = sequelize.define(
-      'Parceiro', {
+        'Parceiro', {
         nome: DataTypes.STRING(150),
         cnpj: DataTypes.STRING(20),
         email: DataTypes.STRING(100),
         senha: DataTypes.STRING(45),
         created_at: DataTypes.DATE,
-     
-      }, {
+
+    }, {
         tableName: "parceiros",
         timestamps: false
-      }
-    );
-  
+    });
+
+    Parceiro.associate = (models) => {
+        Parceiro.hasMany(models.Endereco, { as: "enderecos", foreignKey: "parceiros_id" });
+        Parceiro.hasMany(models.Artigo, { as: "artigos", foreignKey: "parceiros_id" });
+        Parceiro.hasMany(models.Imagem, { as: "imagens", foreignKey: "parceiros_id" });
+
+        Parceiro.belongsToMany(models.Doador, {
+            as: "doacao", 
+            through: "doacoes", 
+            foreignKey: "parceiros_id", 
+            otherKey: "doadores_id", 
+            timestamps: false
+        });
+    };
+
     return Parceiro;
-  }
+}
