904ce649b7b2ffc5c3b4b398532cc81a7e5b63d3
nvmelo <nereuvictor1@gmail.com>
2021-04-22 23:00:07 -0300

    Aplicando indentação e inserindo rotas no Insomnia.json

--- models/Parceiro.js
+++ models/Parceiro.js
@@ -3,10 +3,11 @@ module.exports = (sequelize, DataTypes) => {
     const Parceiro = sequelize.define(
         'Parceiro', {
         nome: DataTypes.STRING(150),
-        cnpj: DataTypes.STRING(20),
+        cnpj: DataTypes.STRING(14),
         email: DataTypes.STRING(100),
         senha: DataTypes.STRING(45),
         created_at: DataTypes.DATE,
+        updated_at: DataTypes.DATE,
 
     }, {
         tableName: "parceiros",
