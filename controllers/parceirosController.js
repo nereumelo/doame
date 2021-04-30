b7fd6c4681223d7d1e6fdcd4cf64e03fc3777507
marianaRodLima <marianarlima93@gmail.com>
2021-04-30 13:36:50 -0300

    Migrando artigos controlles para parceiros controller

--- controllers/parceirosController.js
+++ controllers/parceirosController.js
@@ -1,12 +1,30 @@
-const { Parceiro, Imagem } = require('../models');
+const { Parceiro, Artigo, Imagem, Endereco } = require('../models');
 const bcrypt = require('bcryptjs');
+
+const buscaParceiro = (id) => Parceiro.findByPk(id, ({
+    include: ["enderecos", "imagens", {
+        model: Artigo,
+        as: 'artigos',
+        include: ['imagens']
+    }]
+}));
 
 const parceirosController = {
     index: async (req, res) => {
-        const parceiros = await Parceiro.findAll();
+        const parceiros = await Parceiro.findAll({
+            attributes: ['id','nome', 'cnpj', 'email']
+        });
 
         return res.json(parceiros);
     },
+
+    show: async (req, res) => {
+        const { id } = req.params;
+        const parceiro = await buscaParceiro(id);
+        
+        return res.json(parceiro);
+    },
+
     create: async (req, res) => {
         const { nome, cnpj, email, senha } = req.body;
         const senhaCrypt = bcrypt.hashSync(senha, 10);
@@ -25,9 +43,7 @@ const parceirosController = {
         const newParceiro = req.body;
 
         await Parceiro.update(newParceiro, {
-            where: {
-                id: id
-            }
+            where: { id }
         });
 
         return res.json(newParceiro);
@@ -37,44 +53,136 @@ const parceirosController = {
         const { id } = req.params;
 
         const parceiroDeletado = await Parceiro.destroy({
-            where: {
-                id: id
-            }
+            where: { id }
         });
 
         return res.json(parceiroDeletado);
     },
 
-    //Controler (Imagem)
-    indexImg: async (req, res) => {
-        const { parceiros_id } = req.params;
 
-        const imagens = await Imagem.findAll({
-            attributes: ['id', 'artigos_id','path', 'created_at'],
-            where: {
-                parceiros_id
-                
-            }
+
+    // metodos para endereço
+    createAddress: async(req, res) =>{
+        const { parceiros_id } = req.params; //filtrar a busca pelo id do parceiro
+        const {pais, estado, cidade, bairro, logradouro, cep, numero, complemento } = req.body;
+        
+        const parceiro = await buscaParceiro(parceiros_id);
+        
+        parceiro.enderecos = await Endereco.create({
+
+            parceiros_id,
+            pais,
+            estado,
+            cidade,
+            bairro,
+            logradouro,
+            cep,
+            numero,
+            complemento,
+            
         });
-        return res.json(imagens);
+        return res.json(parceiro);
     },
 
+    updateAddress: async (req, res) => {
+        const { parceiros_id, id } = req.params;
+        const endereco = req.body;
+
+        const parceiro = await buscaParceiro(parceiros_id);
+
+        parceiro.enderecos = await Endereco.update(endereco, {
+            where: { id }
+        });
+
+        return res.send(endereco);
+    },
+
+    deleteAddress: async (req, res) => {
+        const { parceiros_id, id } = req.params;
+        const endereco = req.body;
+
+        const parceiro = await buscaParceiro(parceiros_id);
+
+        parceiro.enderecos = await Endereco.destroy({
+            where: { id }
+        });
+
+        return res.send(endereco); 
+    },
+
+    //Controler (Imagem)
     createImg: async (req, res) => {
         const { parceiros_id } = req.params;
-        const { artigos_id, path } = req.body;
+        const { path } = req.body;
 
-        const novaImagem = await Imagem.create({
+        const parceiro = await buscaParceiro(parceiros_id);
+
+        parceiro.imagens = await Imagem.create({
             parceiros_id,
-            artigos_id,
             path,
         });
         
-        return res.json(novaImagem);
+        return res.json(parceiro);
     },
 
+    deleteImg: async(req, res) => {
+        const { parceiros_id, id } = req.params;
+        const imagem = req.body;
+
+        const parceiro = await buscaParceiro(parceiros_id);
+
+        parceiro.imagens = await Imagem.destroy({
+            where: { id }
+        });
+
+        return res.send(imagem);
+    },
     
+    // Controller (Artigo)
+    createArt: async (req, res) => {
+        const { parceiros_id, titulo, corpo } = req.body;
+        console.log(req.body);
+        
+        const parceiro = await buscaParceiro(parceiros_id)
+        
+        parceiro.artigos = await Artigo.create({
+            parceiros_id,
+            titulo,
+            corpo
+        });
 
+        return res.json(parceiro);
+    },
 
+    updateArt: async (req, res) => {
+        const { parceiros_id, id } = req.params;
+        const newArtigo = req.body;
 
+        const parceiro = await buscaParceiro(parceiros_id);
+        
+        parceiro.artigo = await Artigo.update(newArtigo, {
+            where: {
+                id: id
+            }
+        });
+
+        return res.json(newArtigo);
+    },
+
+    deleteArt: async (req, res) => {
+        const { parceiros_id, id } = req.params;
+        const artigoDel = req.body
+        
+        const parceiro = await buscaParceiro (parceiros_id);
+
+        parceiro.artigos = await Artigo.destroy({
+            where: {
+                id: id
+            }
+        });
+
+        return res.json(artigoDel);
+    },
 }
+
 module.exports = parceirosController;
