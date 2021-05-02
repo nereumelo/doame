83e74913b2cfc85f923796c51ed3ef690e912972
Marina Oliveira <marinaoliv4@gmail.com>
2021-05-02 13:24:09 -0300

    adicionada div com parallax na main; adicionada animação com scroll na class 'porque-doar'

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,10 @@
 window.addEventListener('scroll', function() {
-    let header = document.querySelector('header');
-    let windowPosition = window.scrollY > 0
+        let header = document.querySelector('header');
+        let windowPosition = window.scrollY > 0
 
-    header.classList.toggle('scrolling-ativado', windowPosition);
-})
-
-
-/*JS para modal*/
+        header.classList.toggle('scrolling-ativado', windowPosition);
+    })
+    /*JS para modal*/
 const btnLogin = () => {
     document.querySelector('.modal').style.display = "flex";
 }
@@ -19,3 +17,23 @@ const btnClose = () => {
 const btnCadastro = () => {
     window.open('modalCadastro.html');
 }
+
+//animte-scroll
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(element) {
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass)
+        }
+        console.log(element.offsetTop);
+
+    })
+}
+
+window.addEventListener('scroll', function() {
+    animeScroll();
+})
