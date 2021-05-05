2755cca6cd768d978db9d830a882a7001ea313d6
Marina Oliveira <marinaoliv4@gmail.com>
2021-05-05 17:49:40 -0300

    estilizando menu no header

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,10 +18,10 @@ const btnClose = () => {
 
 }
 
-const nomeUsuario = document.querySelector(".login");
+const nomeUsuario = document.querySelector(".nome-usuario");
 const menu = document.querySelector(".menu");
 nomeUsuario.addEventListener("click", () => {
-    if(menu.style.display == 'none')
+    if (menu.style.display == 'none')
         menu.style.display = "flex";
     else
         menu.style.display = "none";
@@ -33,7 +33,6 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-    console.log('teste')
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
     target.forEach(function(element) {
         if ((windowTop) > element.offsetTop) {
@@ -46,4 +45,8 @@ function animeScroll() {
 
 window.addEventListener('scroll', function() {
     animeScroll();
+})
+
+window.addEventListener('load', function() {
+    login()
 })
