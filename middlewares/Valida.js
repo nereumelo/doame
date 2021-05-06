4584a3be06b3fa99a83b4e9b2a7f52495a1e3cbe
nvmelo <nereuvictor1@gmail.com>
2021-05-06 05:12:45 -0300

    ajustes nos botoes da view perfil

--- middlewares/Valida.js
+++ middlewares/Valida.js
@@ -3,7 +3,6 @@ module.exports = {
         const dados = await req.body;
         const camposVazios = [];
 
-        console.log(dados);
         if (!dados.terms)
             return res.status(400).json({ erro: `Termos de uso e política de privacidade não aceitos.` });
 
